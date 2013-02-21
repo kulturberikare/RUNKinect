33eaa156a8974efd5d871b5ff5a932fa0f88e730
Johan Tidholm <johti626@student.liu.se>
2013-02-21 14:35:42 +0100

    Lägger upp grunden för DepthData

---
+++ KinectKod/DepthDataJT/DepthDataJT/MainWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Microsoft.Kinect;
+
+namespace DepthDataJT
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+
+        #region Members Variables
+        private KinectSensor _Kinect;
+        #endregion Members Variables
+
+        #region Constructor
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.Loaded += (s, e) => { DiscoverKinectSensor(); };
+            this.Unloaded += (s, e) => { this.Kinect = null; };
+        }
+        #endregion Constructor
+
+        #region Methods
+        private void DiscoverKinectSensor()
+        {
+            KinectSensor.KinectSensors.StatusChanged += KinectSensor_StstusChanged;
+            this.Kinect = KinectSensor.KinectSensors
+                                      .FirstOrDefault(x => x.Status == KinectStatus.Connected);
+        }
+
+        private void KinectSensor_StstusChanged(object sender, StatusChangedEventArgs e)
+        {
+            switch (e.Status)
+            {
+                case KinectStatus.Connected:
+                    if (this.Kinect == null)
+                    {
+                        this.Kinect = e.Sensor;
+                    }
+                    break;
+
+                case KinectStatus.Disconnected:
+                    if (this.Kinect == e.Sensor)
+                    {
+                        this.Kinect = null;
+                        this.Kinect = KinectSensor.KinectSensors
+                                                  .FirstOrDefault(x => x.Status == KinectStatus.Connected);
+
+                        if (this.Kinect == null)
+                        {
+                            MessageBox.Show("No connected sensor!");
+                        }
+                    }
+                    break;
+
+                // In case of more statuschanges, add here.
+            }
+        }
+        #endregion Methods
+
+        #region Properties
+        public KinectSensor Kinect
+        {
+            get
+            {
+                return this._Kinect;
+            }
+
+            set
+            {
+                if (this._Kinect != value)
+                {
+                    if (this._Kinect != null)
+                    {
+                        //Unitialize
+                        this._Kinect = null;
+                    }
+                    
+                    if(value != null && value.Status == KinectStatus.Connected)
+                    {
+                        this._Kinect = value;
+                        //Initialize
+                    }
+                }
+            }
+        }
+        #endregion Properties
+    }
+}
