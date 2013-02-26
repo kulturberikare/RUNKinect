fb35069d5df6a8cb7d899af7e99110417a5d2ce1
Johan Tidholm <johti626@student.liu.se>
2013-02-26 14:33:05 +0100

    Tagit tillbaka min Depth data samt lagt till 2 KP
    *2 KP = två kamera program

--- KinectKod/DepthDataJT/DepthDataJT/MainWindow.xaml.cs
+++ KinectKod/DepthDataJT/DepthDataJT/MainWindow.xaml.cs
@@ -26,10 +26,10 @@ namespace DepthDataJT
 
         #region Members Variables
         private KinectSensor _Kinect;
-        private DepthImageFrame _LastDepthFrame;
         private WriteableBitmap _RawDepthImage;
         private Int32Rect _RawDepthImageRect;
-        private int _RawDepthImageStride;
+        private int _RawDepthImagStride;
+        private DepthImageFrame _LastDepthFrame;
         private short[] _DepthImagePixelData;
         #endregion Members Variables
 
@@ -46,12 +46,12 @@ namespace DepthDataJT
         #region Methods
         private void DiscoverKinectSensor()
         {
-            KinectSensor.KinectSensors.StatusChanged += KinectSensor_StatusChanged;
+            KinectSensor.KinectSensors.StatusChanged += KinectSensor_StstusChanged;
             this.Kinect = KinectSensor.KinectSensors
                                       .FirstOrDefault(x => x.Status == KinectStatus.Connected);
         }
 
-        private void KinectSensor_StatusChanged(object sender, StatusChangedEventArgs e)
+        private void KinectSensor_StstusChanged(object sender, StatusChangedEventArgs e)
         {
             switch (e.Status)
             {
@@ -76,7 +76,7 @@ namespace DepthDataJT
                     }
                     break;
 
-                // In case of more statuschanges, add here:
+                // In case of more statuschanges, add here.
             }
         }
 
@@ -90,35 +90,70 @@ namespace DepthDataJT
 
             this._LastDepthFrame = e.OpenDepthImageFrame();
 
-
             if (this._LastDepthFrame != null)
             {
-                CreateBetterShadesOfGray(_LastDepthFrame, _DepthImagePixelData);
-                CreateColorDepthImage(_LastDepthFrame, _DepthImagePixelData);
-                this._LastDepthFrame.CopyPixelDataTo(this._DepthImagePixelData);
+                this._LastDepthFrame.CopyPixelDataTo(_DepthImagePixelData);
                 this._RawDepthImage.WritePixels(this._RawDepthImageRect, this._DepthImagePixelData,
-                                                               this._RawDepthImageStride, 0);
+                                                this._RawDepthImagStride, 0);
+                
+                CreateBetterShadesOfGray(_LastDepthFrame, _DepthImagePixelData);
             }
+            
         }
 
-        private void DepthImage_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        private void DepthImage_MoueLeftButtomUp(object sender, MouseButtonEventArgs e)
         {
             Point p = e.GetPosition(DepthImage);
-
-
             if (this._DepthImagePixelData != null && this._DepthImagePixelData.Length > 0)
             {
-           
-                int pixelIndex  = (int)(p.X + ((int)p.Y * this._LastDepthFrame.Width));
-                int depth       = this._DepthImagePixelData[pixelIndex] >>
-                                 DepthImageFrame.PlayerIndexBitmaskWidth;
+                int pixelIndex = (int)(p.X + ((int)p.Y * this._LastDepthFrame.Width));
+                int depth = this._DepthImagePixelData[pixelIndex] >>
+                            DepthImageFrame.PlayerIndexBitmaskWidth;
                 int depthInches = (int)(depth * 0.0393700787);
-                int depthFt     = depthInches / 12;
-                depthInches     = depthInches % 12;
+                int depthFt = depthInches / 12;
+                depthInches = depthInches % 12;
 
-                PixelDepth.Text =  depth.ToString() +"mm ~"+ depthFt.ToString() +"'"+ depthInches.ToString();
+                PixelDepth.Text = string.Format("{0}mm ~ {1}'{2}\"", depth, depthFt, depthInches);
             }
         }
+
+        //private void CreateColorDepthImage(DepthImageFrame depthFrame, short[] pixelData)
+        //{
+        //    int depth;
+        //    double hue;
+        //    int loThreshold = 1220;
+        //    int hiThreshold = 3048;
+        //    int bytesPerPixel = 4;
+        //    byte[] rgb = new byte[3];
+        //    byte[] enhPixelData = new byte[depthFrame.Width * depthFrame.Height * bytesPerPixel];
+
+        //    for (int i = 0, j = 0; i < pixelData.Length; i++, j += bytesPerPixel)
+        //    {
+        //        depth = pixelData[i] >> DepthImageFrame.PlayerIndexBitmaskWidth;
+
+        //        if (depth < loThreshold || depth > hiThreshold)
+        //        {
+        //            enhPixelData[j] = 0x00;
+        //            enhPixelData[j + 1] = 0x00;
+        //            enhPixelData[j + 2] = 0x00;
+        //        }
+        //        else
+        //        {
+        //            hue= ((360 * depth / 0xFFF) + loThreshold);
+        //            ConvertHslToRgb(hue, 100, 100, rgb);
+
+        //            enhPixelData[j] = rgb[2]; //Blue
+        //            enhPixelData[j + 1] = rgb[1]; //Green
+        //            enhPixelData[j + 2] = rgb[0] //Red
+        //        }
+        //    }
+
+        //    EnhancedDepthImage.Source = BitmapSource.Create(depthFrame.Width, depthFrame.Height,
+        //                                                    96, 96, PixelFormats.Bgr32, null,
+        //                                                    enhPixelData,
+        //                                                    depthFrame.Width *
+        //                                                    bytesPerPixel);
+        //}
 
         private void CreateBetterShadesOfGray(DepthImageFrame depthFrame, short[] pixelData)
         {
@@ -139,125 +174,83 @@ namespace DepthDataJT
                 }
                 else
                 {
-                    gray = (255 * depth / 0xFFF);
+                    gray = (255 * depth / 0xFF);
                 }
 
-                enhPixelData[j]     = (byte)gray;
+                enhPixelData[j] = (byte)gray;
                 enhPixelData[j + 1] = (byte)gray;
                 enhPixelData[j + 2] = (byte)gray;
             }
 
-
-
             EnhancedDepthImage.Source = BitmapSource.Create(depthFrame.Width, depthFrame.Height,
                                                             96, 96, PixelFormats.Bgr32, null,
                                                             enhPixelData,
-                                                            depthFrame.Width * bytesPerPixel);
+                                                            depthFrame.Width *
+                                                            bytesPerPixel);
         }
 
-        private void CreateColorDepthImage(DepthImageFrame depthFrame, short[] pixelData)
+        //private void CreateLighterSahdesOfGray(DepthImageFrame depthFrame, short[] pixelData)
+        //{
+        //    int depth;
+        //    int loThreshold = 1220;
+        //    int hiThreshold = 3048;
+        //    short[] enhPixelData = new short[depthFrame.Width * depthFrame.Height];
+
+        //    for (int i = 0; i < pixelData.Length; i++)
+        //    {
+        //        depth = pixelData[i] >> DepthImageFrame.PlayerIndexBitmaskWidth;
+        //        if (depth < loThreshold || depth > hiThreshold)
+        //        {
+        //            enhPixelData[i] = 0xFF;
+        //        }
+        //        else
+        //        {
+        //            enhPixelData[i] = (short)~pixelData[i];
+        //        }
+        //    }
+
+        //    EnhancedDepthImage.Source = BitmapSource.Create(depthFrame.Width, depthFrame.Height,
+        //                                                    96, 96, PixelFormats.Gray16, null,
+        //                                                    enhPixelData,
+        //                                                    depthFrame.Width * 
+        //                                                    depthFrame.BytesPerPixel);
+        //}
+
+        private void InitializeKinectSensor(KinectSensor sensor)
         {
-            int depth;
-            double hue;
-            int loThreshold     = 1220;
-            int hiThreshold     = 3048;
-            int bytesPerPixel   = 4;
-            byte[] rgb          = new byte[3];
-            byte[] enhPixelData = new byte[depthFrame.Width * depthFrame.Height * bytesPerPixel];
-
-            for (int i = 0, j = 0; i < pixelData.Length; i++, j += bytesPerPixel)
+            if (sensor != null)
             {
-                depth = pixelData[i] >> DepthImageFrame.PlayerIndexBitmaskWidth;
+                DepthImageStream depthStream = sensor.DepthStream;
+                depthStream.Enable();
 
-                if (depth < loThreshold || depth > hiThreshold)
-                {
-                    enhPixelData[j]     = 0x00;
-                    enhPixelData[j + 1] = 0x00;
-                    enhPixelData[j + 2] = 0x00;
-                }
-                else
-                {
-                    hue = ((360 * depth / 0xFFF) + loThreshold);
-                    ConvertHslToRgb(hue, 100, 100, rgb);
+                this._RawDepthImage = new WriteableBitmap(depthStream.FrameWidth,
+                                                          depthStream.FrameHeight, 96, 96,
+                                                           PixelFormats.Gray16, null);
+                this._RawDepthImageRect = new Int32Rect(0, 0, depthStream.FrameWidth,
+                                                        depthStream.FrameHeight);
+                this._RawDepthImagStride = depthStream.FrameWidth * depthStream.FrameBytesPerPixel;
+                DepthImage.Source = this._RawDepthImage;
+                this._DepthImagePixelData = new short[depthStream.FramePixelDataLength];
 
-                    enhPixelData[j]     = rgb[2]; //Blue
-                    enhPixelData[j + 1] = rgb[1]; //Green
-                    enhPixelData[j + 2] = rgb[0]; //Red
-                }
+                MessageBox.Show("Writeble bitmap data:\n" + this._RawDepthImage.PixelWidth.ToString()
+                                + "x" + this._RawDepthImage.PixelHeight.ToString() + "\nRect data:\n"
+                                + _RawDepthImageRect.Width.ToString() + "x" + _RawDepthImageRect.Height.ToString()
+                                + "\nArray pixel data:\nLength:" + _DepthImagePixelData.Length.ToString()
+                                + "\nStride data: \nStride length: " + this._RawDepthImagStride.ToString());
+
+                sensor.DepthFrameReady += Kinect_DepthFrameReady;
+                sensor.Start();
             }
-
-
-
-            EnhancedColorDepthImage.Source = BitmapSource.Create(depthFrame.Width, depthFrame.Height,
-                                                            96, 96, PixelFormats.Bgr32, null,
-                                                            enhPixelData,
-                                                            depthFrame.Width * bytesPerPixel);
         }
 
-        public void ConvertHslToRgb(double hue, double saturation, double lightness, byte[] rgb)
+        private void UninitializeKinectSensor(KinectSensor sensor)
         {
-            double red = 0.0;
-            double green = 0.0;
-            double blue = 0.0;
-            hue = hue % 360.0;
-            saturation = saturation / 100.0;
-            lightness = lightness / 100.0;
-
-            if (saturation == 0.0)
+            if (sensor != null)
             {
-                red = lightness;
-                green = lightness;
-                blue = lightness;
+                sensor.Stop();
+                sensor.DepthFrameReady -= Kinect_DepthFrameReady;
             }
-            else
-            {
-                double huePrime = hue / 60.0;
-                int x = (int)huePrime;
-                double xPrime = huePrime - (double)x;
-                double L0 = lightness * (1.0 - saturation);
-                double L1 = lightness * (1.0 - (saturation * xPrime));
-                double L2 = lightness * (1.0 - (saturation * (1.0 - xPrime)));
-
-                switch (x)
-                {
-                    case 0:
-                        red = lightness;
-                        green = L2;
-                        blue = L0;
-                        break;
-                    case 1:
-                        red = L1;
-                        green = lightness;
-                        blue = L0;
-                        break;
-                    case 2:
-                        red = L0;
-                        green = lightness;
-                        blue = L2;
-                        break;
-                    case 3:
-                        red = L0;
-                        green = L1;
-                        blue = lightness;
-                        break;
-                    case 4:
-                        red = L2;
-                        green = L0;
-                        blue = lightness;
-                        break;
-                    case 5:
-                        red = lightness;
-                        green = L0;
-                        blue = L1;
-                        break;
-                }
-            }
-
-            rgb[0] = (byte)(255.0 * red);
-            rgb[1] = (byte)(255.0 * green);
-            rgb[2] = (byte)(255.0 * blue);
         }
-
         #endregion Methods
 
         #region Properties
@@ -286,35 +279,6 @@ namespace DepthDataJT
                 }
             }
         }
-
-        private void InitializeKinectSensor(KinectSensor sensor)
-        {
-            if (sensor != null)
-            {
-                DepthImageStream depthStream = sensor.DepthStream;
-                depthStream.Enable();
-                this._RawDepthImage = new WriteableBitmap(depthStream.FrameWidth,
-                                                          depthStream.FrameHeight, 96, 96,
-                                                          PixelFormats.Gray16, null);
-                this._RawDepthImageRect = new Int32Rect(0, 0, depthStream.FrameWidth,
-                                                        depthStream.FrameHeight);
-                this._RawDepthImageStride = depthStream.FrameWidth * depthStream.FrameBytesPerPixel;
-                DepthImage.Source = this._RawDepthImage;
-                this._DepthImagePixelData = new short[depthStream.FramePixelDataLength];
-                sensor.DepthFrameReady += Kinect_DepthFrameReady;  
-                sensor.Start();
-            }
-        }
-
-        private void UninitializeKinectSensor(KinectSensor sensor)
-        {
-            if (sensor != null)
-            {
-                sensor.Stop();
-                sensor.DepthFrameReady -= Kinect_DepthFrameReady;
-            }
-        }
-
         #endregion Properties
     }
 }
---
+++ KinectKod/ThePrizeOf1For2/ThePrizeOf1For2/MainWindow.xaml.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Microsoft.Kinect;
+
+namespace ThePrizeOf1For2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Member Variables
+        private KinectSensor _KinectOne;
+        private KinectSensor _KinectTwo;
+        private WriteableBitmap _ColorImageBitmapOne;
+        private Int32Rect _ColorImageBitmapRectOne;
+        private int _ColorImageStrideOne;
+        private WriteableBitmap _ColorImageBitmapTwo;
+        private Int32Rect _ColorImageBitmapRectTwo;
+        private int _ColorImageStrideTwo;
+        #endregion Member Variables
+
+        #region Constructor
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += (s, e) => { DiscoverKinectSensors(); };
+            this.Unloaded += (s, e) => { this.KinectOne = null; this.KinectTwo = null; };
+        }
+        #endregion Constructor
+
+        # region Methods
+        private void DiscoverKinectSensors()
+        {
+            KinectSensor.KinectSensors.StatusChanged += KinectSensors_StatusChanged;
+
+            if (KinectSensor.KinectSensors[0].Status == KinectStatus.Connected)
+            {
+                KinectOne = KinectSensor.KinectSensors[0];
+                MessageBox.Show("Kinect1");
+            }
+            if (KinectSensor.KinectSensors[1].Status == KinectStatus.Connected)
+            {
+                KinectTwo = KinectSensor.KinectSensors[1];
+                MessageBox.Show("Kinect2");
+            }
+        }
+
+        private void KinectSensors_StatusChanged(object sender, StatusChangedEventArgs e)
+        {
+            switch (e.Status)
+            {
+                case KinectStatus.Connected:
+                    if (this.KinectOne == null && e.Sensor == KinectSensor.KinectSensors[0])
+                    {
+                        this.KinectOne = e.Sensor;
+                    }
+                    if (this.KinectTwo == null && e.Sensor == KinectSensor.KinectSensors[1])
+                    {
+                        this.KinectTwo = e.Sensor;
+                    }
+                    break;
+                case KinectStatus.Disconnected:
+                    if (this.KinectOne == e.Sensor && e.Sensor == KinectSensor.KinectSensors[0])
+                    {
+                        this.KinectOne = null;
+                        if (KinectSensor.KinectSensors[0].Status == KinectStatus.Connected)
+                        {
+                            this.KinectOne = KinectSensor.KinectSensors[0];
+                        }
+
+                        if (this.KinectOne == null)
+                        {
+                            MessageBox.Show("Kinect1 dissconected!");
+                        }
+                    }
+                    if (this.KinectTwo == null && e.Sensor == KinectSensor.KinectSensors[1])
+                    {
+                        this.KinectTwo = null;
+                        if (KinectSensor.KinectSensors[1].Status == KinectStatus.Connected)
+                        {
+                            this.KinectTwo = KinectSensor.KinectSensors[1];
+                        }
+
+                        if (this.KinectTwo == null)
+                        {
+                            MessageBox.Show("Kinect2 dissconected!");
+                        }
+                    }
+                    break;
+            }
+        }
+
+        private void InitializeKinectSensorOne(KinectSensor sensor)
+        {
+            if (sensor != null)
+            {
+                ColorImageStream colorStream = sensor.ColorStream;
+                colorStream.Enable();
+                this._ColorImageBitmapOne = new WriteableBitmap(colorStream.FrameWidth, colorStream.FrameHeight,
+                                                                96, 96, PixelFormats.Bgr32, null);
+                this._ColorImageBitmapRectOne = new Int32Rect(0, 0, colorStream.FrameWidth, colorStream.FrameHeight);
+                this._ColorImageStrideOne = colorStream.FrameWidth * colorStream.FrameBytesPerPixel;
+                ColorImageOne.Source = this._ColorImageBitmapOne;
+                sensor.ColorFrameReady += Kinect_ColorFrameReadyOne;
+                sensor.Start();
+                MessageBox.Show("Started kinect1");
+            }
+        }
+
+        private void InitializeKinectSensorTwo(KinectSensor sensor)
+        {
+            if (sensor != null)
+            {
+                ColorImageStream colorStream = sensor.ColorStream;
+                colorStream.Enable();
+                this._ColorImageBitmapTwo = new WriteableBitmap(colorStream.FrameWidth, colorStream.FrameHeight,
+                                                                96, 96, PixelFormats.Bgr32, null);
+                this._ColorImageBitmapRectTwo = new Int32Rect(0, 0, colorStream.FrameWidth, colorStream.FrameHeight);
+                this._ColorImageStrideTwo = colorStream.FrameWidth * colorStream.FrameBytesPerPixel;
+                ColorImageTwo.Source = this._ColorImageBitmapTwo;
+                sensor.ColorFrameReady += Kinect_ColorFrameReadyTwo;
+                sensor.Start();
+                MessageBox.Show("Started kinect2");
+            }
+        }
+
+        private void Kinect_ColorFrameReadyOne(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            using (ColorImageFrame frame = e.OpenColorImageFrame())
+            {
+                if (frame != null)
+                {
+                    byte[] pixelData = new byte[frame.PixelDataLength];
+                    frame.CopyPixelDataTo(pixelData);
+                    this._ColorImageBitmapOne.WritePixels(this._ColorImageBitmapRectOne, pixelData,
+                                                          this._ColorImageStrideOne, 0);
+                }
+            }
+        }
+
+        private void Kinect_ColorFrameReadyTwo(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            using (ColorImageFrame frame = e.OpenColorImageFrame())
+            {
+                if (frame != null)
+                {
+                    byte[] pixelData = new byte[frame.PixelDataLength];
+                    frame.CopyPixelDataTo(pixelData);
+                    this._ColorImageBitmapTwo.WritePixels(this._ColorImageBitmapRectTwo, pixelData,
+                                                          this._ColorImageStrideTwo, 0);
+                }
+            }
+        }
+        #endregion Methods
+
+        #region Properties
+        public KinectSensor KinectOne
+        {
+            get
+            {
+                return _KinectOne;
+            }
+            set
+            {
+                if (this._KinectOne != value)
+                {
+                    if (this._KinectOne != null)
+                    {
+                        //uninitialize Kinect sensor
+                        this._KinectOne = null;
+                    }
+                    if (value != null && value.Status == KinectStatus.Connected)
+                    {
+                        this._KinectOne = value;
+                        InitializeKinectSensorOne(this._KinectOne);
+                    }
+                }
+            }
+        }
+
+        public KinectSensor KinectTwo
+        {
+            get
+            {
+                return _KinectTwo;
+            }
+            set
+            {
+                if (this._KinectTwo != value)
+                {
+                    if (this._KinectTwo != null)
+                    {
+                        //uninitialize Kinect sensor
+                        this._KinectTwo = null;
+                    }
+                    if (value != null && value.Status == KinectStatus.Connected)
+                    {
+                        this._KinectTwo = value;
+                        InitializeKinectSensorTwo(this._KinectTwo);
+                    }
+                }
+            }
+        }
+
+        #endregion Properties
+    }
+}
