fb6b7b2abd4665077596b8c159290dd9275cf516
Johan Tidholm <johti626@student.liu.se>
2013-03-10 13:22:56 +0100

    Kinect info box is here
    Städat lite samt lagt till KinectInfoBox.
    Martin: Fixa Bilddjälen!

---
+++ KinectKod/KinectInfoBoxJT/KinectInfoBoxJT/MainWindow.xaml.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Microsoft.Kinect;
+
+namespace KinectInfoBoxJT
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Member Variables
+        private KinectSensor _Kinect;
+        private MainWindowViewModel viewModel;
+        #endregion Member Variables
+
+        #region Constructor
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            this.Loaded += this.MainWindow_Loaded;
+            this.viewModel = new MainWindowViewModel();
+            this.viewModel.CanStart = false;
+            this.viewModel.CanStop = false;
+            this.DataContext = this.viewModel;
+        }
+        #endregion Constructor
+
+        #region Methods
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (KinectSensor.KinectSensors.Count > 0)
+            {
+                this.Kinect = KinectSensor.KinectSensors
+                    .FirstOrDefault(x => x.Status == KinectStatus.Connected);
+                KinectSensor.KinectSensors.StatusChanged += KinectSensors_StatusChanged;
+                InitializeKinectSensor(this.Kinect);               
+
+                SetKinectInfo();
+            }
+        }
+
+        private void KinectSensors_StatusChanged(object sender, StatusChangedEventArgs e)
+        {
+            this.viewModel.Sensorstatus = e.Status.ToString();
+        }
+
+        private void InitializeKinectSensor(KinectSensor sensor)
+        {
+            if (sensor != null)
+            {
+                this.viewModel.CanStart = false;
+                this.viewModel.CanStop = true;
+                sensor.ColorStream.Enable();
+                sensor.DepthStream.Enable();
+                sensor.SkeletonStream.Enable();
+                sensor.Start();
+            }
+        }
+
+        private void UninitializeKinectSensor(KinectSensor sensor)
+        {
+            if (sensor != null)
+            {
+                this.viewModel.CanStart = true;
+                this.viewModel.CanStop = false;
+                sensor.Stop();
+            }
+        }
+        #endregion Methods
+
+        private void SetKinectInfo()
+        {
+            this.viewModel.ConectionID = this.Kinect.DeviceConnectionId;
+            this.viewModel.DeviceID = this.Kinect.UniqueKinectId;
+            this.viewModel.Sensorstatus = this.Kinect.Status.ToString();
+            this.viewModel.IsColorStreamEnabled = this.Kinect.ColorStream.IsEnabled;
+            this.viewModel.IsDepthStreamEnabled = this.Kinect.DepthStream.IsEnabled;
+            this.viewModel.IsSkeletonStreamEnabled = this.Kinect.SkeletonStream.IsEnabled;
+            this.viewModel.SensorAngle = this.Kinect.ElevationAngle;
+        }
+
+        private void ButtonStart_Click(object sender, RoutedEventArgs e)
+        {
+            this.StartSensor();
+        }
+
+        private void ButtonStop_Click(object sender, RoutedEventArgs e)
+        {
+            this.StopSensor();
+        }
+
+        private void ButtonExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.StopSensor();
+            this.Close();
+
+        }
+
+        private void StartSensor()
+        {
+            if (this.Kinect != null && !this.Kinect.IsRunning)
+            {
+                this.Kinect.Start();
+                this.viewModel.CanStart = false;
+                this.viewModel.CanStop = true;
+            }
+        }
+
+        private void StopSensor()
+        {
+            if (this.Kinect != null && this.Kinect.IsRunning)
+            {
+                this.Kinect.Stop();
+                this.viewModel.CanStart = true;
+                this.viewModel.CanStop = false;
+            }
+        }
+        #region Properties
+        private KinectSensor Kinect
+        {
+            get
+            {
+                return this._Kinect;
+            }
+
+            set
+            {
+                if (this._Kinect != value)
+                {
+                    UninitializeKinectSensor(this.Kinect);
+                    this._Kinect = null;
+                }
+
+                if (value != null && value.Status == KinectStatus.Connected)
+                {
+                    this._Kinect = value;
+                    InitializeKinectSensor(this.Kinect);
+
+                }
+            }
+        }
+        #endregion Properties
+    }
+}
---
+++ KinectKod/KinectInfoBoxJT/KinectInfoBoxJT/MainWindowViewModel.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using System.ComponentModel;
+
+namespace KinectInfoBoxJT
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        #region Member Variables
+        private string sensorStatusValue;
+        private string ConectionIDValue;
+        private string deviceIDValue;
+        private bool isColorStreamEnabledValue;
+        private bool isDepthStreamEnabledValue;
+        private bool isSkeletonStreamEnabledValue;
+        private int sensorAngleValue;
+        private bool canStartValue;
+        private bool canStopValue;
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion Member Variables
+
+        #region Methods
+        public void OnNotifyPropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion Methods
+
+        #region Properties
+        public string Sensorstatus
+        {
+            get
+            {
+                return this.sensorStatusValue;
+            }
+
+            set
+            {
+                if (this.sensorStatusValue != value)
+                {
+                    this.sensorStatusValue = value;
+                    this.OnNotifyPropertyChanged("SensorStatus");
+                }
+            }
+        }
+
+        public string ConectionID
+        {
+            get
+            {
+                return this.ConectionIDValue;
+            }
+
+            set
+            {
+                if (this.ConectionIDValue != value)
+                {
+                    this.ConectionIDValue = value;
+                    this.OnNotifyPropertyChanged("ConectionID");
+                }
+            }
+        }
+
+        public string DeviceID
+        {
+            get
+            {
+                return this.deviceIDValue;
+            }
+
+            set
+            {
+                if (this.deviceIDValue != value)
+                {
+                    this.deviceIDValue = value;
+                    this.OnNotifyPropertyChanged("DeviceID");
+                }
+            }
+        }
+
+        public bool IsColorStreamEnabled
+        {
+            get
+            {
+                return this.isColorStreamEnabledValue;
+            }
+
+            set
+            {
+                if (isColorStreamEnabledValue != value)
+                {
+                    this.isColorStreamEnabledValue = value;
+                    this.OnNotifyPropertyChanged("IsColorstreamEnabled");
+                }
+            }
+        }
+
+        public bool IsDepthStreamEnabled
+        {
+            get
+            {
+                return this.isDepthStreamEnabledValue;
+            }
+
+            set
+            {
+                if (isDepthStreamEnabledValue != value)
+                {
+                    this.isDepthStreamEnabledValue = value;
+                    this.OnNotifyPropertyChanged("IsDepthStreamEnabled");
+                }
+            }
+        }
+
+        public bool IsSkeletonStreamEnabled
+        {
+            get
+            {
+                return this.isSkeletonStreamEnabledValue;
+            }
+            set
+            {
+                if (isSkeletonStreamEnabledValue != value)
+                {
+                    this.isSkeletonStreamEnabledValue = value;
+                    this.OnNotifyPropertyChanged("IsSkeletonStreamEnabled");
+                }
+            }
+        }
+
+        public int SensorAngle
+        {
+            get
+            {
+                return this.sensorAngleValue;
+            }
+
+            set
+            {
+                if (this.sensorAngleValue != value)
+                {
+                    this.sensorAngleValue = value;
+                    this.OnNotifyPropertyChanged("SensorAngle");
+                }
+            }
+        }
+
+        public bool CanStart
+        {
+            get
+            {
+                return this.canStartValue;
+            }
+
+            set
+            {
+                if (this.canStartValue != value)
+                {
+                    this.canStartValue = value;
+                    this.OnNotifyPropertyChanged("CanStart");
+                }
+            }
+        }
+
+        public bool CanStop
+        {
+            get
+            {
+                return this.canStopValue;
+            }
+
+            set
+            {
+                if (this.canStopValue != value)
+                {
+                    this.canStopValue = value;
+                    this.OnNotifyPropertyChanged("CanStop");
+                }
+            }
+        }
+        #endregion Properties
+    }
+}
+
+
