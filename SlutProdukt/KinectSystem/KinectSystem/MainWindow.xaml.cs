cbf99e0c0b87196c8fdf0b9314c5056cbf55dca0
Martin Bäckström <marba607@student.liu.se>
2013-03-27 17:28:04 +0100

    updateringar

--- SlutProdukt/KinectSystem/KinectSystem/MainWindow.xaml.cs
+++ SlutProdukt/KinectSystem/KinectSystem/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Forms;
 
 namespace KinectSystem
 {
@@ -23,6 +24,34 @@ namespace KinectSystem
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void BrowseButton1_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog Info1 = new OpenFileDialog();
+            Info1.InitialDirectory = "c:\\";
+            Info1.Filter = "(*.txt)|*.txt|(*.png)|*.png|(*.jpg)|*.jpg";
+            Info1.RestoreDirectory = true;
+
+            if (Info1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string file = Info1.FileName;
+                InfoLabel1.Content = file;
+            }
+        }
+
+        private void BrowseButton2_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog Info2 = new OpenFileDialog();
+            Info2.InitialDirectory = "c:\\";
+            Info2.Filter = "(*.txt)|*.txt|(*.png)|*.png|(*.jpg)|*.jpg";
+            Info2.RestoreDirectory = true;
+
+            if (Info2.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string file = Info2.FileName;
+                InfoLabel2.Content = file;
+            }
         }
     }
 }
